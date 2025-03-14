57de683215d7f18b61356d7ad5b7b373f2088589
Sohan Rout <sohanrout84@gmail.com>
2025-03-14 23:13:25 +0530

    added long url instead of short

--- src/components/ShortenForm.jsx
+++ src/components/ShortenForm.jsx
@@ -30,7 +30,7 @@ const ShortenForm = ({ setUrls, darkMode }) => {
         {
           longUrl: res.data.longUrl,
           // Create a cleaner shortened URL
-          shortUrl: `linkhive/${shortCode}`, // You can change this to your frontend domain
+          shortUrl: `https://url-shortner-api-rplq.onrender.com/${shortCode}`, // You can change this to your frontend domain
           // Store the full backend URL for actual redirection
           fullUrl: `https://url-shortner-api-rplq.onrender.com/${shortCode}`
         },
